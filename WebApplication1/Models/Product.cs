9ae5242449471630699b04fb69e7f4515f9d58eb
FireKnight76 <fireknight593@gmail.com>
2026-05-24 14:29:35 +0200

    Switched to ASP.NET Core from Visual studio 2026 for WebApp

---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Review> Reviews { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Review.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication1.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Comment { get; set; }
+
+        public int Rating { get; set; }
+
+        public bool IsApproved { get; set; }
+        
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
+
+    }
+}
